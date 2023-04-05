cec5159eb6be6b3ae9344617b4ccce87d4251c50
Shadowman405 <maximus405@gmail.com>
2023-04-05 17:34:16 +0300

    Initial Commit

---
+++ ServiceCalendar/ServiceCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceCalendarApp.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 5.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ServiceCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
