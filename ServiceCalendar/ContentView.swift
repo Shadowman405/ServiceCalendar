d9724c4d74c1df7bcf76c72e4cb581a60d34d8cb
Shadowman405 <maximus405@gmail.com>
2023-04-05 23:15:39 +0300

    minor fixes to UI gradient - change logo

--- ServiceCalendar/ContentView.swift
+++ ServiceCalendar/ContentView.swift
@@ -11,37 +11,50 @@ struct ContentView: View {
     @State private var isPresented = false
     
     var body: some View {
+        
+        
         ZStack {
-            VStack(alignment: .center) {
-                HStack {
-                    Image("main-logo")
-                        .scaledToFill()
-                    .foregroundColor(.accentColor)
+            LinearGradient(colors: [Color.red, Color.purple, Color.blue], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack {
+                VStack(alignment: .center) {
+                    Text("Service Calendar")
+                        .font(.system(size: 36, weight: .heavy, design: .serif))
+                    
+                    HStack {
+                        Image("logo_v2fix")
+                            .scaledToFill()
+                        .foregroundColor(.accentColor)
+                        
+                    }
+                    
+                    Button("Log-In") {
+                        self.isPresented.toggle()
+                    }
+                    .sheet(isPresented: $isPresented, content: {
+                        LoginView()
+                    })
+                    .buttonStyle(.bordered)
+                    .tint(.green)
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    .controlSize(.large)
+                    .font(.system(size: 20, weight: .heavy, design: .serif))
+                    
+                    Button("Register") {
+                        print("Register")
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.red)
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    .controlSize(.large)
+                    .font(.system(size: 20, weight: .heavy, design: .serif))
                 }
-                
-                Button("Log-In") {
-                    self.isPresented.toggle()
-                }
-                .sheet(isPresented: $isPresented, content: {
-                    LoginView()
-                })
-                .buttonStyle(.bordered)
-                .tint(.indigo)
-                .cornerRadius(10)
-                .foregroundColor(.black)
-                .controlSize(.large)
-                
-                Button("Register") {
-                    print("Register")
-                }
-                .buttonStyle(.bordered)
-                .tint(.cyan)
-                .cornerRadius(10)
-                .foregroundColor(.black)
-                .controlSize(.large)
             }
+            .padding()
         }
-        .padding()
     }
 }
 
--- ServiceCalendar/LoginView.swift
+++ ServiceCalendar/LoginView.swift
@@ -14,19 +14,26 @@ struct LoginView: View {
 
     
     var body: some View {
-        VStack{
-            TextField("Username", text: $userName)
-                .textFieldStyle(.roundedBorder)
-                .padding(15)
+        ZStack {
+            LinearGradient(colors: [Color.red, Color.purple, Color.blue], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             
-            TextField("Password", text: $password)
-                .textFieldStyle(.roundedBorder)
-                .padding(15)
-                        
-            Button("Log In") {
-                print("Succesfuly login\n \(userName) - \(password)")
-                dismiss()
+            VStack{
                 
+                TextField("Username", text: $userName)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(15)
+                
+                TextField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(15)
+                            
+                Button("Log In") {
+                    print("Succesfuly login\n \(userName) - \(password)")
+                    dismiss()
+                    
+                }
+                .foregroundColor(.black)
             }
         }
     }
