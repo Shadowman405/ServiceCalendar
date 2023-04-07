5951944ef89ddbbf709b79dab06129c3d24a041b
Shadowman405 <maximus405@gmail.com>
2023-04-07 20:39:13 +0300

    add groups some new logic

---
+++ ServiceCalendar/View/CarCell.swift
@@ -0,0 +1,20 @@
+//
+//  CarCell.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 7.04.23.
+//
+
+import SwiftUI
+
+struct CarCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CarCell()
+    }
+}
