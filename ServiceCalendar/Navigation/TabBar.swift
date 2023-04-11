735f308f2f2f87ea83af63e6cb3fbb206aaab151
Shadowman405 <maximus405@gmail.com>
2023-04-11 22:16:17 +0300

    configure tab bar

--- ServiceCalendar/Navigation/TabBar.swift
+++ ServiceCalendar/Navigation/TabBar.swift
@@ -11,9 +11,29 @@ struct TabBar: View {
     var body: some View {
         ZStack {
             HStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Button {
+                    print("Beep")
+                } label: {
+                    Image(systemName: "mappin.and.ellipse")
+                        .frame(width: 44,height: 44)
+                }
+                
+                Spacer()
+
+                NavigationLink {
+                    
+                } label: {
+                    Image(systemName: "list.star")
+                        .frame(width: 44,height: 44)
+                }
+
             }
+            .font(.title2)
+            .foregroundColor(.blue)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
         }
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
     }
 }
 
