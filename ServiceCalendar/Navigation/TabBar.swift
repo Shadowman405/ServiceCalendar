8b9737e4de197c2bc83fda8de4105b12c5e4d4c7
Shadowman405 <maximus405@gmail.com>
2023-04-11 22:11:39 +0300

    add tab bar view

---
+++ ServiceCalendar/Navigation/TabBar.swift
@@ -0,0 +1,24 @@
+//
+//  TabBar.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 11.04.23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        ZStack {
+            HStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
