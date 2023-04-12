fd563b547048fbff1be7bd574bdf443a5639d510
Shadowman405 <maximus405@gmail.com>
2023-04-12 22:14:19 +0300

    add new shape for toolbar

---
+++ ServiceCalendar/Utils/Shapes.swift
@@ -0,0 +1,17 @@
+//
+//  Shapes.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 12.04.23.
+//
+
+import SwiftUI
+
+struct Arc: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        
+        return path
+    }
+}
