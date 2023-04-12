712d5c0b1bc89f9ccf93ee92261c80b8aecc04d3
Shadowman405 <maximus405@gmail.com>
2023-04-12 22:16:47 +0300

    configure Arc shape

--- ServiceCalendar/Utils/Shapes.swift
+++ ServiceCalendar/Utils/Shapes.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct Arc: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.minY), control: CGPoint(x: rect.midX, y: rect.midY))
         
         return path
     }
