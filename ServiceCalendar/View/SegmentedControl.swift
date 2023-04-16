c72462d78f761c169679d99dcc2b7800691ec3ba
Shadowman405 <maximus405@gmail.com>
2023-04-16 13:45:33 +0300

    configure segmented control

--- ServiceCalendar/View/SegmentedControl.swift
+++ ServiceCalendar/View/SegmentedControl.swift
@@ -31,7 +31,25 @@ struct SegmentedControl: View {
 
             }
             .font(.subheadline.weight(.semibold))
-            .foregroundColor(.black)
+            .foregroundColor(.white)
+            
+            //MARK: - Sepearator
+            Divider()
+                .background(.white.opacity(0.5))
+                .blendMode(.overlay)
+                .shadow(color: .black.opacity(0.2), radius: 0,x: 0,y: 1)
+                .blendMode(.overlay)
+                .overlay {
+                    HStack {
+                        //MARK: - Underline
+                        Divider()
+                            .frame(width: UIScreen.main.bounds.width / 2,height: 3)
+                            .background(.white)
+                        .blendMode(.overlay)
+                    }
+                    .frame(maxWidth: .infinity, alignment: selection == 0 ? .leading : .trailing)
+                    .offset(y: -1)
+                }
         }
         .padding(.top, 25)
     }
