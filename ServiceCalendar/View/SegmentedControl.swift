dc29caf5b03d3010fb6f665e7d22b0878139ec43
Shadowman405 <maximus405@gmail.com>
2023-04-16 13:37:21 +0300

    add segmented control to selected car view

--- ServiceCalendar/View/SegmentedControl.swift
+++ ServiceCalendar/View/SegmentedControl.swift
@@ -8,22 +8,27 @@
 import SwiftUI
 
 struct SegmentedControl: View {
+    @Binding var selection: Int
+    
+    
     var body: some View {
         VStack(spacing: 5) {
             HStack {
                 Button {
-                    
+                    selection = 0
                 } label: {
                     Text("Service")
                 }
-                
-                Spacer()
+                .frame(minWidth: 0, maxWidth: .infinity)
 
+                
                 Button {
-                    
+                    selection = 1
                 } label: {
                     Text("Spended Money")
                 }
+                .frame(minWidth: 0, maxWidth: .infinity)
+
             }
             .font(.subheadline.weight(.semibold))
             .foregroundColor(.black)
@@ -34,6 +39,6 @@ struct SegmentedControl: View {
 
 struct SegmentedControl_Previews: PreviewProvider {
     static var previews: some View {
-        SegmentedControl()
+        SegmentedControl(selection: .constant(0))
     }
 }
