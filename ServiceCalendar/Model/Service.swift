7d90458c05d5f019f94cf95eb744266350e7f3a8
Shadowman405 <maximus405@gmail.com>
2023-04-18 12:30:23 +0300

    create struct Service
    simple model

--- ServiceCalendar/Model/Service.swift
+++ ServiceCalendar/Model/Service.swift
@@ -11,3 +11,11 @@ enum ServiceSegmentedControl {
     case service
     case spendedMoney
 }
+
+struct Service: Identifiable {
+    var id = UUID()
+    var mileage: Int
+    var date: Date
+    var doneService: Bool
+    var checkMoney: Int
+}
