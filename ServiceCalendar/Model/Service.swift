3620a38909eaea1c9571b88ff4aa7ad6f8625cb6
Shadowman405 <maximus405@gmail.com>
2023-04-18 12:26:50 +0300

    add new model for scroll view

---
+++ ServiceCalendar/Model/Service.swift
@@ -0,0 +1,13 @@
+//
+//  Service.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 18.04.23.
+//
+
+import Foundation
+
+enum ServiceSegmentedControl {
+    case service
+    case spendedMoney
+}
