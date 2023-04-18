6cb9641d81b30fcbf3a1f8047b22783cd6cf6473
Shadowman405 <maximus405@gmail.com>
2023-04-18 12:37:17 +0300

    fulfill with mock data

--- ServiceCalendar/Model/Service.swift
+++ ServiceCalendar/Model/Service.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ServiceSegmentedControl {
+enum ServiceSegmentedControlModel {
     case service
     case spendedMoney
 }
@@ -18,4 +18,14 @@ struct Service: Identifiable {
     var date: Date
     var doneService: Bool
     var checkMoney: Int
+}
+
+extension ServiceSegmentedControlModel {
+    static let mockService: [Service] = [
+        Service(mileage: 220000, date: .init(timeIntervalSinceNow: 0), doneService: true, checkMoney: 150),
+        Service(mileage: 230000, date: .init(timeIntervalSinceNow: 0), doneService: false, checkMoney: 110),
+        Service(mileage: 250000, date: .init(timeIntervalSinceNow: 0), doneService: false, checkMoney: 940),
+        Service(mileage: 260000, date: .init(timeIntervalSinceNow: 0), doneService: false, checkMoney: 100),
+        Service(mileage: 264000, date: .init(timeIntervalSinceNow: 0), doneService: false, checkMoney: 66)
+    ]
 }
