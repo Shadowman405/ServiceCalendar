02b485ef9f184319dfb4707a368713a240b5bf81
Shadowman405 <maximus405@gmail.com>
2023-04-20 22:24:21 +0300

    new file with custom Nav Bar

---
+++ ServiceCalendar/Navigation/NavigationBar.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBar.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 20.04.23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+    }
+}
