bd17522dfc0f0ac0981409cfe03111eba2008594
Shadowman405 <maximus405@gmail.com>
2023-04-22 21:12:10 +0300

    add new view for services

---
+++ ServiceCalendar/View/ServicesView.swift
@@ -0,0 +1,20 @@
+//
+//  ServicesView.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 22.04.23.
+//
+
+import SwiftUI
+
+struct ServicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ServicesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ServicesView()
+    }
+}
