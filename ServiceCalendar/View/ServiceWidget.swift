7fc40702c17dffdba85689ad55a2572928035815
Shadowman405 <maximus405@gmail.com>
2023-04-26 21:51:08 +0300

    create new file for widget

---
+++ ServiceCalendar/View/ServiceWidget.swift
@@ -0,0 +1,20 @@
+//
+//  ServiceWidget.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 26.04.23.
+//
+
+import SwiftUI
+
+struct ServiceWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ServiceWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        ServiceWidget()
+    }
+}
