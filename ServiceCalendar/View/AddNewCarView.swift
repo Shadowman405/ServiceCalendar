83c00d64aff4dc7fc581a9e8b6b74ddaa29f811a
Shadowman405 <maximus405@gmail.com>
2023-05-09 16:50:58 +0300

    preparing view to photo picker

--- ServiceCalendar/View/AddNewCarView.swift
+++ ServiceCalendar/View/AddNewCarView.swift
@@ -12,29 +12,40 @@ struct AddNewCarView: View {
     @State private var carModel : String = ""
     @State private var carMileage : String = ""
     
+    
     var body: some View {
         ZStack {
-            VStack(alignment: .leading,spacing: 10) {
-                HStack {
-                    Text("Enter Car mark")
-                        .padding()
-                    Spacer()
-                    TextField("Mark...", text: $carMark)
+            LinearGradient(colors: [Color.blue, Color.purple], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                
+                VStack(alignment: .leading,spacing: 10) {
+                    HStack {
+                        Text("Enter Car mark")
+                            .padding()
+                        Spacer()
+                        TextField("Mark...", text: $carMark)
+                    }
+                    HStack {
+                        Text("Enter Car model")
+                            .padding()
+                        Spacer()
+                        TextField("Model...", text: $carModel)
+                    }
+                    HStack {
+                        Text("Enter Car mileage")
+                            .padding()
+                        Spacer()
+                        TextField("Mileage...", text: $carMileage)
+                            .keyboardType(.numberPad)
+                    }
+                    HStack {
+                        Text("Choose Car photos")
+                            .padding()
+                    }
                 }
-                HStack {
-                    Text("Enter Car model")
-                        .padding()
-                    Spacer()
-                    TextField("Model...", text: $carModel)
-                }
-                HStack {
-                    Text("Enter Car mileage")
-                        .padding()
-                    Spacer()
-                    TextField("Mileage...", text: $carMileage)
-                        .keyboardType(.numberPad)
-                }
-                Text("Choose Car photos")
             }
         }
     }
