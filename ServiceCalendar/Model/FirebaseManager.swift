8ce0d7f2ba4a3f9612b95675cef72e945981d4c9
Shadowman405 <maximus405@gmail.com>
2023-05-12 14:43:48 +0300

    add firestorm link in  code

--- ServiceCalendar/Model/FirebaseManager.swift
+++ ServiceCalendar/Model/FirebaseManager.swift
@@ -11,12 +11,15 @@ import FirebaseFirestore
 
 class FirebaseManager: NSObject {
     let auth: Auth
+    let firestore: Firestore
+    
     static let shared = FirebaseManager()
     
     override init() {
         FirebaseApp.configure()
         
         self.auth = Auth.auth()
+        self.firestore = Firestore.firestore()
         
         super.init()
     }
