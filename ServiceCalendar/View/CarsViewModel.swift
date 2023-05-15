2a7afc16729cd938394853e636facd7a5884c142
Shadowman405 <maximus405@gmail.com>
2023-05-15 23:06:32 +0300

    looks like create func to get data from DB

--- ServiceCalendar/View/CarsViewModel.swift
+++ ServiceCalendar/View/CarsViewModel.swift
@@ -14,5 +14,15 @@ class CarsViewModel: ObservableObject {
     
     private func fetchCars() {
         guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return}
+        
+        FirebaseManager.shared.firestore.collection("users").document(uid).collection("cars").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = snapshot?.data() else { return}
+            print(data)
+        }
     }
 }
