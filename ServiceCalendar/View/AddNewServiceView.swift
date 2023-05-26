e160a785f28fb28a5844f064b8c0c6082811c716
Shadowman405 <maximus405@gmail.com>
2023-05-26 22:22:59 +0300

    add back colors

--- ServiceCalendar/View/AddNewServiceView.swift
+++ ServiceCalendar/View/AddNewServiceView.swift
@@ -14,27 +14,32 @@ struct AddNewServiceView: View {
     @State private var checkMoney = ""
     
     var body: some View {
-        VStack(alignment: .leading,spacing: 10) {
-            HStack {
-                Text("Enter Mileage")
-                    .padding()
-                Spacer()
-                TextField("Mileage...", text: $mileage)
-            }
-            HStack {
-//                Text("Enter date")
-//                    .padding()
-//                Spacer()
-                DatePicker("Select Date", selection: $date)
-                    .padding()
-                    .datePickerStyle(.compact)
-            }
-            HStack {
-                Text("Enter cost of service")
-                    .padding()
-                Spacer()
-                TextField("Service cost...", text: $checkMoney)
-                    .keyboardType(.numberPad)
+        ZStack {
+            LinearGradient(colors: [Color.clear, Color.blue, Color.purple], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading,spacing: 10) {
+                HStack {
+                    Text("Enter Mileage")
+                        .padding()
+                    Spacer()
+                    TextField("Mileage...", text: $mileage)
+                }
+                HStack {
+    //                Text("Enter date")
+    //                    .padding()
+    //                Spacer()
+                    DatePicker("Select Date", selection: $date)
+                        .padding()
+                        .datePickerStyle(.compact)
+                }
+                HStack {
+                    Text("Enter cost of service")
+                        .padding()
+                    Spacer()
+                    TextField("Service cost...", text: $checkMoney)
+                        .keyboardType(.numberPad)
+                }
             }
         }
     }
