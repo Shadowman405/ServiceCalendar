9044798e141f3d757d3ce9b4f82a2886ba2bbaae
Shadowman405 <maximus405@gmail.com>
2023-06-11 19:41:36 +0300

    trying to add scroll view

--- ServiceCalendar/View/ForecastView.swift
+++ ServiceCalendar/View/ForecastView.swift
@@ -55,12 +55,14 @@ struct ForecastView: View {
                 .padding(.horizontal, 5)
                 
                 
-                ServicesCostAllWidget(descriptionText: "Spended money\n on service for all time:", sumText: "\(serviceType.allTimeCostOfServices(services: selectedServices ?? Service.mockService))", services: selectedServices ?? Service.mockService)
-                    .padding(.top ,35)
-                    .padding(.leading, -175)
-                
-                ServicesCostAllWidget(descriptionText: "Spended money\n on gasoline for all time:", sumText: "\(serviceType.allTimeCostOfGasoline(services: selectedServices ?? Service.mockService))", services: selectedServices ?? Service.mockService)
-                    .padding()
+                ScrollView(.horizontal) {
+                    ServicesCostAllWidget(descriptionText: "Spended money\n on service for all time:", sumText: "\(serviceType.allTimeCostOfServices(services: selectedServices ?? Service.mockService))", services: selectedServices ?? Service.mockService)
+                        .padding(.top ,35)
+                        .padding(.leading, -175)
+                    
+                    ServicesCostAllWidget(descriptionText: "Spended money\n on gasoline for all time:", sumText: "\(serviceType.allTimeCostOfGasoline(services: selectedServices ?? Service.mockService))", services: selectedServices ?? Service.mockService)
+                        .padding()
+                }
             }
             
         }
