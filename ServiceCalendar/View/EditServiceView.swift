0582ed810f7ede25fa0d7799ab3babbaed83cd86
Shadowman405 <maximus405@gmail.com>
2023-06-20 20:44:31 +0300

    add new view for editing Service

---
+++ ServiceCalendar/View/EditServiceView.swift
@@ -0,0 +1,20 @@
+//
+//  EditServiceView.swift
+//  ServiceCalendar
+//
+//  Created by Maxim Mitin on 20.06.23.
+//
+
+import SwiftUI
+
+struct EditServiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditServiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditServiceView()
+    }
+}
