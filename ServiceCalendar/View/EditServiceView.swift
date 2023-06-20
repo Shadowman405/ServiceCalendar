6c0307eef47f14daadfb5b3173ad802e7a3c96b6
Shadowman405 <maximus405@gmail.com>
2023-06-20 21:43:59 +0300

    minor fixes and adding save button without logic

--- ServiceCalendar/View/EditServiceView.swift
+++ ServiceCalendar/View/EditServiceView.swift
@@ -23,36 +23,44 @@ struct EditServiceView: View {
         ZStack {
             LinearGradient(colors: [Color.clear,Color.blue, Color.purple], startPoint: .top, endPoint: .bottom)
             
-            Form{
-                Section("Mileage") {
-                    TextField("mileage", text: $mileage)
-                }
-                Section("Date") {
-                    DatePicker("Select Date", selection: $date)
-                        .datePickerStyle(.compact)
-                }
-                Section("Is service Done?") {
-                    Toggle("Is service done?", isOn: $isDone)
-                }
-                Section("Check Money") {
-                    TextField("mileage", text: $checkMoney)
-                }
-                Section("Service type") {
-                    Picker("Choose type of service", selection: $serviceType) {
-                        ForEach(typeOfServices, id: \.self) {
-                            Text($0)
-                        }
+            VStack {
+                Form{
+                    Section("Mileage") {
+                        TextField("mileage", text: $mileage)
                     }
-                    .pickerStyle(.segmented)
+                    Section("Date") {
+                        DatePicker("Select Date", selection: $date)
+                            .datePickerStyle(.compact)
+                    }
+                    Section("Is service Done?") {
+                        Toggle("Is service done?", isOn: $isDone)
+                            .accentColor(.green)
+                    }
+                    Section("Check Money") {
+                        TextField("mileage", text: $checkMoney)
+                    }
+                    Section("Service type") {
+                        Picker("Choose type of service", selection: $serviceType) {
+                            ForEach(typeOfServices, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Section("Service Description") {
+                        TextField("mileage", text: $serviceDescription, axis: .vertical)
+                            .lineLimit(200)
+                            .background(Color.green.opacity(0.2))
+                    }
+                    Button("Save") {
+                        print("Suck bebra")
+                    }
+                    .foregroundColor(.green)
                 }
-                Section("Service Description") {
-                    TextField("mileage", text: $serviceDescription, axis: .vertical)
-                        .lineLimit(200)
-                        .background(Color.green.opacity(0.2))
-                }
+                .padding()
+                .cornerRadius(20)
+                
             }
-            .padding()
-            .cornerRadius(20)
         }
         .ignoresSafeArea()
     }
