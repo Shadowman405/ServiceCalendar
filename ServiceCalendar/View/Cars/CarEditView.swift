3224d61501020e778d44e9949f494e4256160ec2
Shadowman405 <maximus405@gmail.com>
2023-07-03 18:35:07 +0300

    looks like I need another view model

--- ServiceCalendar/View/Cars/CarEditView.swift
+++ ServiceCalendar/View/Cars/CarEditView.swift
@@ -19,7 +19,67 @@ struct CarEditView: View {
     @State var imagesArray : [String] = []
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(colors: [Color.clear, Color.blue, Color.purple], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                PhotosPicker(selection: $carPhoto) {
+                    Text("Add photo")
+                        .foregroundColor(.orange)
+                }
+                .onChange(of: carPhoto) { newItem in
+                    for item in carPhoto {
+                        Task{
+                            if let data = try? await item.loadTransferable(type: Data.self){
+                                if let uiImage = UIImage(data: data){
+                                    selectedImages.append(uiImage)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                if !selectedImages.isEmpty {
+                    TabView {
+                        ForEach(0..<selectedImages.count, id: \.self) { i in
+                            Image(uiImage: selectedImages[i])
+                                .resizable()
+                                .frame(height: 320)
+                                .cornerRadius(30)
+                                .scaledToFit()
+                                .padding()
+                        }
+                    }
+                    .frame(height: 320)
+                    .tabViewStyle(.page)
+                } else {
+                    Image(systemName: "photo.on.rectangle")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                }
+                
+                VStack(alignment: .leading,spacing: 10) {
+                    Form {
+                        Section(header: Text("Enter car mark")) {
+                            TextField("Car mark...", text: $carMark)
+                                .keyboardType(.numberPad)
+                        }
+                        Section(header: Text("Enter car model")) {
+                            TextField("Car model...", text: $carModel)
+                                .keyboardType(.numberPad)
+                        }
+                        Section(header: Text("Enter car mileage")) {
+                            TextField("Mileage...", text: $carMileage)
+                                .keyboardType(.numberPad)
+                        }
+                    }
+                    .frame(height: UIScreen.main.bounds.height/3)
+                    .cornerRadius(20)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
